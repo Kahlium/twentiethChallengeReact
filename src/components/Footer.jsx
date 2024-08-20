0fd5ca744287fb38a7113dfe9bbdb7aaf9d260ba
Kahlium <kahligtz@gmail.com>
2024-08-20 17:49:18 -0600

    footer looks less dumb

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,14 @@
 export default function Footer() {
     return (
-      <footer>Karen Gutierrez</footer>
+      <div>
+        <div className='container profile-footer'>
+          <div className='row p-2'>
+            <footer>Karen Gutierrez</footer>
+            <a href='https://www.linkedin.com/in/karen-gutierrez-749792150/'>linkedIn</a>
+            <a href='https://github.com/Kahlium'>GitHub</a>
+          </div>
+        </div>
+      </div>
     );
   }
   
